3f6dc09acd0c25a057b7cdee279d80a8d63582d4
Stefan Rothe <stefan.rothe@gymkirchenfeld.ch>
2021-05-08 12:47:28 +0200

    Set version to 5.0

--- smartglove_neo/config.h
+++ smartglove_neo/config.h
@@ -17,10 +17,10 @@
 
 //#define DEBUG
 
-#define VERSION "v4.3"
+#define VERSION "v5.0"
 
-#define VERSION_MAJOR 4
-#define VERSION_MINOR 3
+#define VERSION_MAJOR 5
+#define VERSION_MINOR 0
 
 #define I2C_FLEX_INDEX_FINGER_ADDRESS        0x12
 #define I2C_FLEX_MIDDLE_FINGER_ADDRESS       0x13
